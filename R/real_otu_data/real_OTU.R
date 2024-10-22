2945483b46a3193d691f39837bd81c8af2935074
Evan Gorstein <egorstein@wisc.edu>
2024-10-22 19:05:41 -0400

    changes to how we analyze real otu data

--- R/real_otu_data/real_OTU.R
+++ R/real_otu_data/real_OTU.R
@@ -1,83 +1,51 @@
-#rm(list=ls())
-#library(edgeR)
-#library(scran)
-library(dplyr)
+#___________________________________
+# Pre-processing of real OTU dataset
+#___________________________________
+
+# Load data ---------------------------------------------------------------
 load("data/real/OTU/ageset.Rdata")
-
 OTU_original <- data.obj$otu.tab
-OTU_original <- t(OTU_original)
-#filter OTU
+OTU_original <- t(OTU_original) # samples by OTUs
+dim(OTU_original)
 
-# Filter based on OTU prevalence
+#  Filter OTU to remove less informative OTUs and reduce dimension --------
+# We imposed two filters:
+# (1) OTU prevalence < 10%
 prevalence_threshold <- 0.1
 OTU_prevalence <- apply(OTU_original != 0, 2, mean)
 OTU_keep <- OTU_prevalence >= prevalence_threshold
-
-# Filter based on median non-zero counts
+# (2) Median non-zero counts < 10.
 median_count_threshold <- 10
 median_counts <- apply(OTU_original, 2, function(x) median(x[x != 0], na.rm = TRUE))
 OTU_keep <- OTU_keep & median_counts >= median_count_threshold
-
-# Subset the OTU table based on the filtered columns
+# Perform filtering
 OTU_filtered <- OTU_original[, OTU_keep]
 dim(OTU_filtered)
-# calculate 97% quantile for each taxon
-quantiles <- apply(OTU_filtered, 2, quantile, probs = 0.97)
-
-# Replace values greater than the 97th percentile with the 97th percentile value
-for (i in 1:ncol(OTU_filtered)) {
-  OTU_filtered[OTU_filtered[,i] > quantiles[i], i] <- quantiles[i]
-}
-dim(OTU_filtered)
-
-OTU_filtered = sqrt(OTU_filtered)
-
-OTU_filtered_N<- t(apply(OTU_filtered, 1, function(OTU_filtered) OTU_filtered/sum(OTU_filtered)))
 
 
-OTU = OTU_filtered_N
+# Add pseudo counts and normalize with centered log ratio transfor --------
+OTU_filtered_pc <- OTU_filtered
+OTU_filtered_pc[OTU_filtered_pc == 0] <- 0.5
+OTU_filtered_n <- sweep(log(OTU_filtered_pc),
+                        MARGIN=1,
+                        STATS=rowMeans(log(OTU_filtered_pc)) )
 
 
+# Form dataframe ----------------------------------------------------------
 meta_data <- data.obj$meta.dat
-age <- meta_data['age']
-age <-sqrt(age)
-age <- data.frame(data_name = rownames(age), age = age$age)
-OTU<-data.frame(data_name = rownames(OTU),OTU)
-# Merge the data frames
-merged_data <- merge(OTU, age, by = "data_name")
+country = meta_data$geo_loc_name
+# Square-root transformation of the age
+sqrt_age <- sqrt(meta_data$age)
+df <- data.frame(country, OTU_filtered_n, sqrt_age) |>
+  tibble::rownames_to_column("sample_id")
+# Drop observations with missing age
+df_complete <- tidyr::drop_na(df, sqrt_age)
+dim(df_complete) # dropped 40 observations
 
-
-
-country = meta_data['geo_loc_name']
-# Create a new column that maps each country name to an integer value
-country$geo_loc_name <- ifelse(country$geo_loc_name == "USA", 1, 
-                               ifelse(country$geo_loc_name == "Malawi", 2,
-                                      ifelse(country$geo_loc_name == "Venezuela", 3, NA)))
-
-country$geo_loc_name = as.factor(country$geo_loc_name)
-country <- data.frame(data_name = rownames(country), country = country$geo_loc_name)
-merged_data2 <- merge(country,merged_data, by = "data_name")
-
-
-#merged_data2 <- merged_data2 %>% select(-data_name)
-# Delete the column named "data_name"
-merged_data2  <- merged_data2 [, !colnames(merged_data2 ) %in% "data_name"]
-
-
-dim(merged_data2)
-
-merged_data2 <- merged_data2[complete.cases(merged_data2$age), ]
-dim(merged_data2)
-
-
-data = merged_data2
-
-write.csv(data, "data/real/OTU/tss_normalized_data.csv", row.names = F)
-
-# get taxonomies of OTUs included in this final data set
-OTU_ids = colnames(data)
-OTU_ids = OTU_ids[2:(length(OTU_ids)-1)]
-OTU_taxonomies = data.obj$otu.name
-final_OTU_taxonomies = OTU_taxonomies[str_remove(OTU_ids, "X"),]
-write.csv(final_OTU_taxonomies, "data/real/OTU/taxonomies.csv", row.names = T)
+# Write processed data and OTU phylo info matrix --------------------------
+write.csv(df_complete, "data/real/OTU/normalized_data.csv", row.names = F)
+OTU_kept_ids = colnames(OTU_filtered)
+OTU_phylo = data.obj$otu.name
+OTU_kept_phylo = OTU_phylo[OTU_kept_ids,]
+write.csv(OTU_kept_phylo, "data/real/OTU/final_taxonomies.csv", row.names = T)
 
--- scripts/real_OTU.jl
+++ scripts/real_OTU.jl
@@ -1,35 +1,54 @@
 using HighDimMixedModels
-using RCall
+using StatsBase
+using JLD2
 using CSV
 using DataFrames
-using Lasso
-using MLBase
-using Random
-using JLD2
+using Plots
+using RCall
 
 # get data
 # note that response y is square root of age and the design matrix is library size normalized
 data_dir = "data/real/OTU"
-d = CSV.read("$data_dir/tss_normalized_data.csv", DataFrame)
-otu_taxonomies = CSV.read("$data_dir/taxonomies.csv", DataFrame)
+d = CSV.read("$data_dir/normalized_data.csv", DataFrame)
+otu_taxonomies = CSV.read("$data_dir/final_taxonomies.csv", DataFrame)
 N = size(d)[1]
-grp = d.country
+grp = convert(Vector{String}, d.country)
 # USA: 1, Malawi: 2, Venezuela: 3
 countmap(grp) 
-otus = Matrix(d[:,2:(end-1)])
-y = d.age
+otus = Matrix(d[:,3:(end-1)])
+y = d.sqrt_age
 X = ones(N, 1)
 
 control = Control()
 control.trace = 3
-λ = 75
-control.tol = 1e-3
-Random.seed!(1234)
-# We choose to standardize because the data has been library sum scaled and so is very small numbers
-@time int_fit = hdmm(X, otus, y, grp; 
-    penalty="scad", λ=λ, ψstr="ident", control=control)
+log_y = log.(y)
+int_fit_orig = hdmm(
+X, otus, log_y, grp;
+penalty="scad", λ=200, ψstr="ident", control=control
+)
 
+# check diagnostics
+scatter(int_fit_orig.fitted, int_fit_orig.resid)
+scatter(otus[:,365], log_y)
+mask_outly = int_fit_orig.resid .> 1.5
+scatter(int_fit_orig.fitted, log_y)
+annotate!(
+int_fit_orig.fitted[mask_outly].+0.01, 
+log.(y)[mask_outly], 
+text.(d.sample_id[mask_outly], :red, :left, 11)
+)
+
+
+# fit model with dropped otus and with quadratic for 365th otu
+otus = [otus otus[:,365].^2] 
+@time int_fit = hdmm(
+X[.!mask_outly,:], otus[.!mask_outly,:], log_y[.!mask_outly], grp[.!mask_outly]; 
+penalty="scad", λ=200, ψstr="ident", control=control
+)
+scatter(int_fit.fitted, int_fit.resid)
+scatter(int_fit.fitted, log_y[.!mask_outly])
 save_object("data/real/OTU/otu_fit.jld2", int_fit)
+
 
 
 # load otu_fit.jld2
@@ -38,7 +57,7 @@ otu_fit = load_object("data/real/OTU/otu_fit.jld2")
 
 
 # get indices of non-zero coefficients besides intercept in the initial estimates
-init_effects = otu_fit.init_coef.βstart[2:end]
+init_effects = otu_fit.init_coef.βstart[2:end-1]
 idx_nz_init = findall(!iszero, init_effects)
 # get taxonomies of non-zero coefficients and their values
 coef_nz_init = [otu_taxonomies[idx_nz_init,end-3:end] init_effects[idx_nz_init]] 
@@ -47,13 +66,14 @@ max_coef = maximum(abs.(coef_nz_init[:,end]))
 min_coef = minimum(abs.(coef_nz_init[:,end]))
 
 # get indices of non-zero coefficients besides intercept in the final estimates
-effects = otu_fit.fixef[2:end]
+effects = otu_fit.fixef[2:end-1]
 idx_nz = findall(!iszero, effects)
 # get names of non-zero coefficients and their values
 coef_nz = [idx_nz otu_taxonomies[idx_nz, (end-2):end] ]
 coef_nz.effect_estimates = effects[idx_nz]
 rename!(coef_nz, "x1" => "OTU_index")
 R"
+library(tinytable)
 $coef_nz |>
     tt(digits = 2) |>
     style_tt(align = \"c\") |>
@@ -61,7 +81,7 @@ $coef_nz |>
 "
 
 #Calculate variance explained
-raw_var = var(y)
+raw_var = var(log_y)
 resid_var = var(otu_fit.resid)
 # same as otu_fit.σ²
 var_exp = 1 - resid_var/raw_var
